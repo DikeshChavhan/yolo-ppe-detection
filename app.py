2017d8760665ad666a8a8d61ae4f69909f9fe93f
Dikesh Vishnu Chavhan <167902807+DikeshChavhan@users.noreply.github.com>
2025-12-30 00:09:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from ultralytics import YOLO
 import streamlit as st
 from PIL import Image
-import numpy as np
-import cv2
-import tempfile
+from utils import draw_results
 
+st.set_page_config(page_title="PPE Detection App", layout="centered")
+st.title("🦺 PPE Detection using YOLO11")
+
+model = YOLO("best.pt")
+
+uploaded = st.file_uploader("Upload an image", type=["jpg","png","jpeg"])
+
+if uploaded:
+    image = Image.open(uploaded)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    st.write("🔍 Running detection...")
+    results = model(image)[0]
+
+    output = draw_results(results)
+    st.image(output, caption="Prediction", use_column_width=True)
+
+    st.success("✅ Done")
+else:
+    st.info("Upload an image to start.")
